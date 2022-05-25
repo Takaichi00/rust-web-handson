f64a534b22e2ba97ca21c3b5849a7bc975a84e01
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2022-05-25 11:01:44 +0900

    feat: 201 created のときに response json を返すように修正

--- rust-web-handson-presentation/src/controller/todo.rs
+++ rust-web-handson-presentation/src/controller/todo.rs
@@ -3,7 +3,7 @@ use std::{sync::Arc, ptr::null};
 use axum::{http::StatusCode, response::IntoResponse, routing::{get, post}, Extension, Json, Router};
 use rust_web_handson_app::modules::{UseCaseModules, UseCaseModulesExt};
 
-use crate::model::todo::TodoJson;
+use crate::model::{todo::TodoJson, todo_create_response::TodoCreateResponseJson};
 
 // insert をするときは route を追加 → 対応するメソッドを追加する
 pub fn router() -> Router {
@@ -35,7 +35,9 @@ pub async fn get_all(
 
 pub async fn create() -> Result<impl IntoResponse, StatusCode> {
     if true {
-        return Ok((StatusCode::CREATED));
+        let mockResponse: TodoCreateResponseJson = TodoCreateResponseJson::new(1, "title".to_string(), "description".to_string(), "2022-01-01 01:00:00".to_string()).await;
+        let body: Json<TodoCreateResponseJson> = Json(mockResponse);
+        return Ok((StatusCode::CREATED, body));
     }
 
     return Err((StatusCode::INTERNAL_SERVER_ERROR));
@@ -44,17 +46,29 @@ pub async fn create() -> Result<impl IntoResponse, StatusCode> {
 #[cfg(test)]
 mod tests {
     
+    use std::fmt::Debug;
+
+    use axum::body::HttpBody;
+
+    use crate::model::todo_create_response::TodoCreateResponseJson;
+
     use super::*;
 
     #[tokio::test]
     async fn createが正常に成功した場合はStatusCode_CREATEDが取得できる() {
-        let expected = StatusCode::CREATED;
+        let expectedStatus = StatusCode::CREATED;
+        // let expectedJson = TodoCreateResponseJson::new(1, "title".to_string(), "description".to_string(), "2022-01-01 01:00:00".to_string());
         let actual = create().await.ok();
     
         match actual {
             Some(s) => {
-                let statusCode = s.into_response().status();
-                assert_eq!(statusCode, expected);
+                let actualStatusCode = s.into_response().status();
+                assert_eq!(actualStatusCode, expectedStatus);
+                
+                // Response Body をどう取得するか?
+
+                // let actualJson = s.into_response().into_body().boxed().
+                // print!("{}", actualJson);
             },
             None => unreachable!(),
         }
---
+++ rust-web-handson-presentation/src/model/todo_create_response.rs
@@ -0,0 +1,33 @@
+// use rust_web_handson_domain::model::todo::Todo;
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct TodoCreateResponseJson {
+    id: i64,
+    title: String,
+    description: String,
+    created_at: String,
+}
+
+impl TodoCreateResponseJson {
+    pub async fn new (id: i64, title: String, description: String, created_at: String) -> Self {
+
+        Self {
+            id,
+            title,
+            description,
+            created_at
+        }
+    }
+}
+
+// impl From<Todo> for TodoCreateResponseJson {
+//     fn from(t: Todo) -> Self {
+//         TodoJson {
+//             id: t.id,
+//             title: t.title,
+//             description: t.description,
+//             created_at: t.created_at.to_string(),
+//         }
+//     }
+// }

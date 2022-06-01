5aeffd8465ed8ef58a78362ccd0dbff0deb86e30
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2022-06-01 11:08:30 +0900

    test: it で header の検証を追加

--- integration-test/src/main.rs
+++ integration-test/src/main.rs
@@ -4,14 +4,12 @@ fn main() {
 }
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
 
     use rust_web_handson_presentation::model::todo_create_response::TodoCreateResponseJson;
 
     #[tokio::test]
     async fn _201_todoを正常に作成することができる() -> Result<(), Box<dyn std::error::Error>> {
 
-        // TODO json を作って頑張ってアサーションする? それとも text → json のパースを頑張るか
         let client = reqwest::Client::new();
         let res = client.post("http://127.0.0.1:8080/todo/try")
             .body("{ \"title\": \"hogehgoe\", \"description\": \"fugafuga\" }")
@@ -21,23 +19,19 @@ mod tests {
             // 以下、i64 → String の キャストは勝手にやってくれなさそう
             // .json::<HashMap<String, String>>()
 
+        assert_eq!(res.status().as_u16(), 201);
+        assert_eq!(res.headers().contains_key("Location"), true);
+        assert_eq!(res.headers().get("Location").unwrap(), &"http://localhost:8080/todo/1");
+
+        // テストケースごとにテストデータを用意できれば id なども検証できる (やる必要があるかは考えどころ)
         let response_json = res.json::<TodoCreateResponseJson>().await?;
         assert_eq!(response_json.title, "hogehoge");
         assert_eq!(response_json.description, "fugafuga");
-        // res.status();
-        // assert_eq!(res.status().clone(), 201);
-
-        // let resp = reqwest::get("https://httpbin.org/ip")
-        // .await?
-        // .json::<HashMap<String, String>>()
-        // .await?;
-
-        // println!("{:#?}", res);
         Ok(())
     }
 
     /**
-     * Blocking のテスト実行↓
+     * Blocking のテスト実行例
      */
     #[test]
     fn sample_e2e_test_blocking() -> Result<(), Box<dyn std::error::Error>> {

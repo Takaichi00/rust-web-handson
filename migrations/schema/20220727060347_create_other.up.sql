22d075ada730ff3b591c7abc789ea72142db5edc
YAMADA Kazuaki <iterable-company@gmail.com>
2022-07-27 15:11:59 +0900

    add others schema

--- migrations/schema/20220727060347_create_other.up.sql
+++ migrations/schema/20220727060347_create_other.up.sql
@@ -1,1 +1,10 @@
 -- Add up migration script here
+create table others (
+    id bigint auto_increment primary key,
+    title varchar(255),
+    description text,
+    fuga varchar(255),
+    created_at datetime default current_timestamp,
+    updated_at datetime default current_timestamp on update current_timestamp,
+    deleted_at datetime default null
+) character set utf8mb4 collate utf8mb4_bin;

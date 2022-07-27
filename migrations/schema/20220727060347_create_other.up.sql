c542c7171dff157447196951208ae7a5bebc7a46
YAMADA Kazuaki <iterable-company@gmail.com>
2022-07-27 15:26:28 +0900

    test

--- migrations/schema/20220727060347_create_other.up.sql
+++ migrations/schema/20220727060347_create_other.up.sql
@@ -1,10 +1,12 @@
 -- Add up migration script here
 create table others (
     id bigint auto_increment primary key,
+    todo_id bigint,
     title varchar(255),
     description text,
     fuga varchar(255),
     created_at datetime default current_timestamp,
     updated_at datetime default current_timestamp on update current_timestamp,
-    deleted_at datetime default null
+    deleted_at datetime default null,
+    constraint foreign key fk_todo_id (todo_id) references todo(id)
 ) character set utf8mb4 collate utf8mb4_bin;
